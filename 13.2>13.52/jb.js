68f11c8052127a91c6a04cf075a8b581264763c8
mgstoree <ninjaa11@proton.me>
2026-09-20 20:38:04 +0300

    Update jb.js

--- 13.2>13.52/jb.js
+++ 13.2>13.52/jb.js
@@ -1,9 +1,3 @@
-body {
-    background-image: url("img.jpg");
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-}
 import { establishPrimitive } from "./core.js?v=10";
 import { installWindowP, pairStatus } from "./mem.js";
 import { int64 } from "./int64.js";
