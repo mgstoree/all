b729c1468eee02fc5dd51cbe9d70484494aab624
mgstoree <ninjaa11@proton.me>
2026-09-20 19:37:27 +0300

    Update jb.js

--- 13.2>13.52/jb.js
+++ 13.2>13.52/jb.js
@@ -1,8 +1,4 @@
-<!DOCTYPE html>
-<html><head>
-<title> مع MG هتلاقي اللى نفسك فيه 😉</title>
-<link rel="stylesheet" href="style.css">
-</head></html>
+
 import { establishPrimitive } from "./core.js?v=10";
 import { installWindowP, pairStatus } from "./mem.js";
 import { int64 } from "./int64.js";
